c5e199194befffecbb6eeba1937faf7a51e26625
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-07-07 14:27:49 +0200

    Update entrypoint.sh

--- pyspark/entrypoint.sh
+++ pyspark/entrypoint.sh
@@ -19,22 +19,22 @@
 # echo commands to the terminal output
 set -ex
 
-# # Check whether there is a passwd entry for the container UID
-# myuid=$(id -u)
-# mygid=$(id -g)
-# # turn off -e for getent because it will return error code in anonymous uid case
-# set +e
-# uidentry=$(getent passwd $myuid)
-# set -e
+# Check whether there is a passwd entry for the container UID
+myuid=$(id -u)
+mygid=$(id -g)
+# turn off -e for getent because it will return error code in anonymous uid case
+set +e
+uidentry=$(getent passwd $myuid)
+set -e
 
-# # If there is no passwd entry for the container UID, attempt to create one
-# if [ -z "$uidentry" ] ; then
-#     if [ -w /etc/passwd ] ; then
-#         echo "$myuid:x:$myuid:$mygid:${SPARK_USER_NAME:-anonymous uid}:$SPARK_HOME:/bin/false" >> /etc/passwd
-#     else
-#         echo "Container ENTRYPOINT failed to add passwd entry for anonymous UID"
-#     fi
-# fi
+# If there is no passwd entry for the container UID, attempt to create one
+if [ -z "$uidentry" ] ; then
+    if [ -w /etc/passwd ] ; then
+        echo "$myuid:x:$myuid:$mygid:${SPARK_USER_NAME:-anonymous uid}:$SPARK_HOME:/bin/false" >> /etc/passwd
+    else
+        echo "Container ENTRYPOINT failed to add passwd entry for anonymous UID"
+    fi
+fi
 
 if [ -z "$JAVA_HOME" ]; then
   JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' | awk '{print $3}')
