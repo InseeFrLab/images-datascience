63743e1d0d791a11f68a3bb8286bebf3efd619aa
Alexis D <alexis.dondon@insee.fr>
2022-08-26 18:42:17 +0200

    test

--- matrix copy.sh
+++ matrix copy.sh
@@ -1,6 +1,6 @@
 buildargs="--build-arg BASE_IMAGE="
 python_version=""
-if [ -n "$python_version" ]; then
+if [ -z "$python_version" ]; then
     buildargs+=" --build-arg PYTHON_VERSION=$python_version"
 fi
 echo "${buildargs}"
