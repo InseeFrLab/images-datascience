46e279c7fd4f32c0dedd2b60777c6f2f99a844ed
Alexis D <alexis.dondon@insee.fr>
2022-08-30 11:26:32 +0200

    test

--- matrix.sh
+++ matrix.sh
@@ -2,38 +2,14 @@
 base_image=image1,image2
 python_version=v1,v2
 context=python
+a=ubuntu:20.04
+b=nvidia/cuda:11.3.1-base-ubuntu20.04
 
-result=""
-if [[ "$context" =~ ^(base)$ ]]; then
-    echo "$context is in the list"
-    IFS=', ' read -r -a array <<< "$base_image"
-    for element in "${array[@]}"
-    do
-        JSON_STRING=$( jq -n \
-                  --arg bn "$element" \
-                  '{base_image: $bn}' )
-        echo $JSON_STRING
-        result+=("$JSON_STRING")
-    done
-fi
-
-if [[ "$context" =~ ^(python)$ ]]; then
-    echo "$context is in the list"
-    IFS=', ' read -r -a array <<< "$base_image"
-    IFS=', ' read -r -a python_version_array <<< "$python_version"
-    for element in "${array[@]}"
-    do
-      for pv in "${python_version_array[@]}"
-      do
-        JSON_STRING=$( jq -n \
-                  --arg bn "$element" \
-                  --arg pv "$pv" \
-                  '{base_image: $bn, python_version_array: $pv}' )
-        echo $JSON_STRING
-        result+="$JSON_STRING,"
-      done
-    done
-fi
-
-r=${result::-1}
-echo "{"inputs":[$r]
+base_image_escaped=$(echo $a  | sed 's/\//\\\//g' )
+echo $base_image_escaped
+base_image_gpu_escaped=$(echo $b | sed 's/\//\\\//g' )
+echo $base_image_gpu_escaped
+sed -i "s/:base_image_gpu/$base_image_gpu_escaped/g" ./releases/base.json
+sed -i "s/:base_image/$base_image_escaped/g" ./releases/base.json
+matrix=$(cat ./releases/base.json | jq .) 
+echo $matrix   
