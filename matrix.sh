30fb7a287cdbee28d8fa0b7aee34627fe7a7a3d9
Alexis D <alexis.dondon@insee.fr>
2022-09-02 06:04:03 +0200

    test

--- matrix.sh
+++ matrix.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 FILES="./releases/*"
+data=[]
 for f in $FILES
 do
 	echo "Processing $f"
-    jq '.[] | .artefact_output_name' $f
+    array=$(jq '.[] | .artefact_output_name' $f)
+    for a in ${array[@]}; do
+        echo "$a"
+        data=$(echo $data | jq ". += [{"image":$a}]")
+    done
 done
+echo $data
