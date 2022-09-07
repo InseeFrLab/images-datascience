a144215df32cd0e31ecce5386279777d280b56b1
avouacr <avouacr@pm.me>
2022-09-07 16:23:24 +0000

    harmonize quarto version

---
+++ base/common-scripts/install-quarto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install latest version of quarto
+QUARTO_DL_URL=$(wget -qO- https://quarto.org/docs/download/_download.json | grep -oP '(?<=\"download_url\":\s\")https.*linux-amd64.deb')
+wget -q ${QUARTO_DL_URL} -O quarto.deb
+dpkg -i quarto.deb
+quarto check install
+
+rm quarto.deb
