584a1a6271f5c4329b6863855400903ac063386d
avouacr <avouacr@pm.me>
2022-09-09 12:29:05 +0000

    dev

--- dev/build-chain.py
+++ dev/build-chain.py
@@ -2,20 +2,23 @@ import subprocess
 import sys
 
 chains = {
-    "r": ["base", "r-minimal", "r-datascience", "rstudio"],
-    "python": ["base", "python-minimal", "python-datascience", "jupyter"]
+    "rstudio": ["base", "r-minimal", "r-datascience", "rstudio"],
+    "rstudio-sparkr": ["base", "r-minimal", "spark", "rstudio"],
+    "jupyter": ["base", "python-minimal", "python-datascience", "jupyter"],
+    "jupyter-pyspark": ["base", "python-minimal", "spark", "jupyter"],
+    "vscode": ["base", "python-minimal", "python-datascience", "vscode"],
 }
 
 chain = chains[sys.argv[1]]
 
 for i, image in enumerate(chain):
     if image == "base":
-        cmd = ["docker", "build", image, "-t", image, "--build-arg", "BASE_IMAGE=ubuntu:20.04"]
+        cmd = ["sudo", "docker", "build", image, "-t", image, "--build-arg", "BASE_IMAGE=ubuntu:20.04"]
         print(" ".join(cmd))
         subprocess.run(cmd)
     else:
         previous_image = chain[i-1]
-        cmd = ["docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"]
+        cmd = ["sudo", "docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"]
         print(" ".join(cmd))
         subprocess.run(cmd)
         if len(sys.argv) == 2 and image == sys.argv[2]:
