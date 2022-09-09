fd18640793675812246550bf9023b965e7f5a8fc
avouacr <avouacr@pm.me>
2022-09-09 12:17:22 +0000

    dev

--- dev/build-chain.py
+++ dev/build-chain.py
@@ -10,9 +10,13 @@ chain = chains[sys.argv[1]]
 
 for i, image in enumerate(chain):
     if image == "base":
-        subprocess.run(["docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE=ubuntu:20.04"])
+        cmd = ["docker", "build", image, "-t", image, "--build-arg", "BASE_IMAGE=ubuntu:20.04"]
+        print(" ".join(cmd))
+        subprocess.run(cmd)
     else:
         previous_image = chain[i-1]
-        subprocess.run(["docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"])
+        cmd = ["docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"]
+        print(" ".join(cmd))
+        subprocess.run(cmd)
         if len(sys.argv == 2) and image == sys.argv[2]:
             break
