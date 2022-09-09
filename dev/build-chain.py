b4de5fbcc72ec101a9e42b21dfc887f1cfda529e
avouacr <avouacr@pm.me>
2022-09-09 14:07:43 +0200

    build-all script

---
+++ dev/build-chain.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+
+chains = {
+    "r": ["base", "r-minimal", "r-datascience", "rstudio"],
+    "python": ["base", "python-minimal", "python-datascience", "jupyter"]
+}
+
+chain = chains[sys.argv[1]]
+
+for i, image in enumerate(chain):
+    if image == "base":
+        subprocess.run(["docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE=ubuntu:20.04"])
+    else:
+        previous_image = chain[i-1]
+        subprocess.run(["docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"])
+        if len(sys.argv == )
