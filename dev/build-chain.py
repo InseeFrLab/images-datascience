367c103dc78b7216e7018458b77439ccb89a98f3
avouacr <avouacr@pm.me>
2022-09-09 13:53:33 +0000

    dev

--- dev/build-chain.py
+++ dev/build-chain.py
@@ -21,5 +21,6 @@ for i, image in enumerate(chain):
         cmd = ["sudo", "docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"]
         print(" ".join(cmd))
         subprocess.run(cmd)
-        if len(sys.argv) == 2 and image == sys.argv[2]:
-            break
+        if len(sys.argv) == 2:
+            if image == sys.argv[2]:
+                break
