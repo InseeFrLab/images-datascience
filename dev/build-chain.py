f59f3f5d1eb54f6ce87ad586ccecc03a028fdf47
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-09-14 15:13:32 +0200

    Add jupyter-r service (#34)
    * put common scripts in standalone dir
    
    * install r and julia in jupyter dockerfile
    
    * test dev workflow to propagate tags other than latest
    
    * fix missing fi
    
    * build-all script
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * add jdemetra+
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * back to standalone julia install
    
    * no tag for branch events
    
    * add jupyter-r
    
    * dev
    
    * dev
    
    * dev
    
    * handle GPU in dev pipeline
    
    * dev
    
    * dev
    
    * dev
    
    * workflows

---
+++ dev/build-chain.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+
+
+chains = {
+    "rstudio": ["base", "r-minimal", "r-datascience", "rstudio"],
+    "rstudio-sparkr": ["base", "r-minimal", "spark", "rstudio"],
+    "jupyter-r": ["base", "r-minimal", "r-datascience", "jupyter-r"],
+    "jupyter-python": ["base", "python-minimal", "python-datascience",
+                       "jupyter-python"],
+    "jupyter-pyspark": ["base", "python-minimal", "spark", "jupyter"],
+    "vscode": ["base", "python-minimal", "python-datascience", "vscode"],
+}
+
+chain = chains[sys.argv[1]]
+
+GPU = False
+if len(sys.argv) == 3:
+    if sys.argv[2] == "gpu":
+        GPU = True
+
+for i, image in enumerate(chain):
+
+    if image == "base":
+        previous_image = "ubuntu:20.04"
+    else:
+        previous_image = chain[i-1]
+
+    if GPU:
+        device_suffix = "-gpu"
+    else:
+        device_suffix = ""
+
+    cmd = ["sudo", "docker", "build", image, "-t", image,
+           "--build-arg", f"BASE_IMAGE={previous_image}",
+           "--build-arg", f"DEVICE_SUFFIX={device_suffix}"]
+    print(" ".join(cmd))
+    subprocess.run(cmd)
