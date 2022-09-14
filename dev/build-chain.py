3bbd63cc3f57dbf7b06767e5989e29a0875f7e9c
avouacr <avouacr@pm.me>
2022-09-14 09:37:07 +0000

    handle GPU in dev pipeline

--- dev/build-chain.py
+++ dev/build-chain.py
@@ -1,27 +1,39 @@
 import subprocess
 import sys
 
+
 chains = {
     "rstudio": ["base", "r-minimal", "r-datascience", "rstudio"],
     "rstudio-sparkr": ["base", "r-minimal", "spark", "rstudio"],
     "jupyter-r": ["base", "r-minimal", "r-datascience", "jupyter-r"],
-    "jupyter-python": ["base", "python-minimal", "python-datascience", "jupyter-python"],
+    "jupyter-python": ["base", "python-minimal", "python-datascience",
+                       "jupyter-python"],
     "jupyter-pyspark": ["base", "python-minimal", "spark", "jupyter"],
     "vscode": ["base", "python-minimal", "python-datascience", "vscode"],
 }
 
+GPU = False
+
 chain = chains[sys.argv[1]]
 
 for i, image in enumerate(chain):
+
     if image == "base":
-        cmd = ["sudo", "docker", "build", image, "-t", image, "--build-arg", "BASE_IMAGE=ubuntu:20.04"]
-        print(" ".join(cmd))
-        subprocess.run(cmd)
+        previous_image = "ubuntu:20.04"
     else:
         previous_image = chain[i-1]
-        cmd = ["sudo", "docker", "build", image, "-t", image, "--build-arg", f"BASE_IMAGE={previous_image}"]
-        print(" ".join(cmd))
-        subprocess.run(cmd)
-        if len(sys.argv) == 3:
-            if image == sys.argv[2]:
-                break
+
+    if GPU:
+        device_suffix = "-gpu"
+    else:
+        device_suffix = ""
+
+    cmd = ["sudo", "docker", "build", image, "-t", image,
+           "--build-arg", f"BASE_IMAGE={previous_image}",
+           "--build-arg", f"DEVICE_SUFFIX={device_suffix}"]
+    print(" ".join(cmd))
+    subprocess.run(cmd)
+
+    if len(sys.argv) == 3:
+        if image == sys.argv[2]:
+            break
