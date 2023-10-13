f42c3a999e684ae53b8f8028f1a89c01babf7ead
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-13 18:02:04 +0200

    Update onyxia-init.sh

--- scripts/onyxia-init.sh
+++ scripts/onyxia-init.sh
@@ -150,8 +150,10 @@ if command -v R; then
     env | grep "IMAGE_NAME" >> ${R_HOME}/etc/Renviron.site
 fi
 
-if command -v jupyter lab; then
-    echo "{\"@jupyterlab/apputils-extension:themes\": {\"theme\": \"JupyterLab Dark\"}}" > /opt/mamba/share/jupyter/lab/settings/overrides.json;
+if [[ -n "$DARK_MODE" ]]; then 
+    if command -v jupyter lab; then
+        echo "{\"@jupyterlab/apputils-extension:themes\": {\"theme\": \"JupyterLab Dark\"}}" > /opt/mamba/share/jupyter/lab/settings/overrides.json;
+    fi
 fi
 
 if [[ -e "$HOME/work" ]]; then
