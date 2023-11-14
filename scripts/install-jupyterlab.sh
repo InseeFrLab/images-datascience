cdb9c689865cbdb205e5a509860b05cc59b5b5bf
avouacr <avouacr@pm.me>
2023-11-14 17:43:19 +0000

    fix: use pip3 instead of pip to handle case when conda is not installed

--- scripts/install-jupyterlab.sh
+++ scripts/install-jupyterlab.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# If Conda is installed, use it to install Jupyterlab
 if command -v mamba ; then
     mamba install -y jupyterlab
 else
-# Else, install via pip
-    pip install jupyterlab
+    pip3 install jupyterlab
 fi
