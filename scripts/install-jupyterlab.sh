7776a1af65f65abd6a37e8a640036b2ebe675e98
avouacr <avouacr@pm.me>
2023-11-20 10:53:56 +0000

    fix(jupyter): handle case when pip is not installed

--- scripts/install-jupyterlab.sh
+++ scripts/install-jupyterlab.sh
@@ -4,5 +4,9 @@ set -e
 if command -v mamba ; then
     mamba install -y jupyterlab
 else
+    if [ "`which pip3`" = "" ]; then
+        apt-get update
+        apt-get install -y --no-install-recommends python3-pip
+    fi
     pip3 install jupyterlab
 fi
