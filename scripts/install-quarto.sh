a638a1539177bf3f2c938fd76bb87bd0a8eb2797
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-12-08 17:53:47 +0100

    install tinytex via quarto (#164)

--- scripts/install-quarto.sh
+++ scripts/install-quarto.sh
@@ -7,3 +7,6 @@ wget -q ${QUARTO_DL_URL} -O quarto.deb
 dpkg -i quarto.deb
 quarto check install
 rm quarto.deb
+
+# Install TinyTeX to enable rendering PDF documents via quarto
+quarto install tinytex --update-path
