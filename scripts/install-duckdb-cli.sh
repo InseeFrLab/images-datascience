040f65822845ba29cbfab29895395459950fc58c
fcomte <frederic.comte@insee.fr>
2024-01-17 22:30:26 +0000

    multi

--- scripts/install-duckdb-cli.sh
+++ scripts/install-duckdb-cli.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -e
 
-wget -q https://github.com/duckdb/duckdb/releases/latest/download/duckdb_cli-linux-amd64.zip
-unzip duckdb_cli-linux-amd64.zip -d /usr/local/bin/
-rm duckdb_cli-linux-amd64.zip
+ARCH=$(uname -m)
+
+case $ARCH in
+    "x86_64")
+        FILENAME="duckdb_cli-linux-amd64.zip"
+        ;;
+    "aarch64")
+        FILENAME="duckdb_cli-linux-aarch64.zip"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+wget -q https://github.com/duckdb/duckdb/releases/latest/download/$FILENAME
+unzip $FILENAME -d /usr/local/bin/
+rm $FILENAME
 duckdb -c "SET extension_directory=\"${HOME}\" ; INSTALL httpfs; INSTALL aws;"
