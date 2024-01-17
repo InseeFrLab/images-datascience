1921cbfaf88639337810101c166240e944aa21f8
fcomte <frederic.comte@insee.fr>
2024-01-17 22:42:26 +0000

    vault arch

--- scripts/install-vault-cli.sh
+++ scripts/install-vault-cli.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -e
 
+ARCH=$(uname -m)
+
+case $ARCH in
+    "x86_64")
+        ARCHITECTURE="amd64"
+        ;;
+    "aarch64")
+        ARCHITECTURE="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
 VAULT_LATEST_VERSION=$(curl --silent "https://api.github.com/repos/hashicorp/vault/releases/latest" | grep -Po '"tag_name": "v\K.*?(?=")')
-wget -q https://releases.hashicorp.com/vault/${VAULT_LATEST_VERSION}/vault_${VAULT_LATEST_VERSION}_linux_amd64.zip -O vault.zip
+wget -q https://releases.hashicorp.com/vault/${VAULT_LATEST_VERSION}/vault_${VAULT_LATEST_VERSION}_linux_${ARCHITECTURE}.zip -O vault.zip
 unzip vault.zip -d /usr/local/bin/
 vault -autocomplete-install
 rm vault.zip
