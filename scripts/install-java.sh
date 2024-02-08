cdeb4864c323514646e795f57cbe906e5797ce8e
odysseu <55583423+odysseu@users.noreply.github.com>
2024-02-08 09:37:47 +0100

    added ENV in dockerfiles so vars are passed to containers

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -6,10 +6,6 @@ apt-get install -y --no-install-recommends \
     openjdk-${JAVA_VERSION}-jre-headless \
     openjdk-${JAVA_VERSION}-jdk-headless
 
-JAVA_HOME="/usr/lib/jvm/java-$JAVA_VERSION-openjdk-amd64"
-
-PATH="${JAVA_HOME}/bin:${PATH}"
-
 if command -v R; then
     R CMD javareconf
 fi
