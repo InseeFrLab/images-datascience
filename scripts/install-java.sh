d171cfd84cd34556e46085a3735c7b50f5753787
avouacr <avouacr@pm.me>
2024-02-09 09:46:04 +0000

    refactor

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+apt-get update
 apt-get install -y --no-install-recommends \
     ca-certificates-java \
+    libbz2-dev \
     openjdk-${JAVA_VERSION}-jre-headless \
-    openjdk-${JAVA_VERSION}-jdk-headless \
-    libbz2-dev # for jdk
+    openjdk-${JAVA_VERSION}-jdk-headless
 
 if command -v R; then
     R CMD javareconf
