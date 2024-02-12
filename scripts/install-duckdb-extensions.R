f73b74767398de9628a5916d10cdfb893f60fc6a
Pierre Lamarche <pierre.lamarche@insee.fr>
2024-02-12 15:28:18 +0100

    Add postgres extension for duckdb (#183)

--- scripts/install-duckdb-extensions.R
+++ scripts/install-duckdb-extensions.R
@@ -2,4 +2,5 @@ con <- DBI::dbConnect(duckdb::duckdb())
 
 DBI::dbExecute(con, 'INSTALL httpfs')
 DBI::dbExecute(con, 'INSTALL aws')
+DBI::dbExecute(con, 'INSTALL postgres')
 DBI::dbDisconnect(con, shutdown=TRUE)
