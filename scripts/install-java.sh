7dea08bbe6ca5f941c2340fb398ad583f2f0ae17
avouacr <avouacr@pm.me>
2024-02-20 15:05:04 +0000

    fix(scripts): apt_install must be defined in each script

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-apt-get update
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
 apt_install \
     ca-certificates-java \
     libbz2-dev \
--- scripts/install-system-libs-R.sh
+++ scripts/install-system-libs-R.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-source ./utils.sh
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
 
 apt_install \
     libglpk40 \
