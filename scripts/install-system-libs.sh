d9b111531dec39b0b94342498463502bcd6b257d
avouacr <avouacr@pm.me>
2024-03-01 19:25:15 +0000

    fix order

--- scripts/install-system-libs.sh
+++ scripts/install-system-libs.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# PPA for postgresql-client
-echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
 function apt_install() {
     if ! dpkg -s "$@" >/dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -27,10 +23,14 @@ apt_install \
     lsb-core \
     nano \
     openssh-client \
-    postgresql-client \
     python3-pip \
     sudo \
     tini \
     unzip \
     vim \
     wget
+
+# Install postgresql-client
+echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt_install postgresql-client
