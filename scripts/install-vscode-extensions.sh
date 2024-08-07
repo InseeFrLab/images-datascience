0bf54ca7e5e5fe2d57ec01db653eb2678f247ff9
avouacr <avouacr@pm.me>
2024-08-07 15:53:32 +0000

    feat(vscode): retry mechanism for extensions installation

---
+++ scripts/install-vscode-extensions.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Number of retries
+retries=3
+# Wait time between retries
+wait_time=10
+
+# Function to install an extension with retries
+install_extension() {
+    local extension=$1
+    local attempt=1
+
+    while [ $attempt -le $retries ]; do
+        code-server --install-extension $extension && break
+        echo "Failed to install $extension. Attempt $attempt/$retries. Retrying in $wait_time seconds..."
+        attempt=$((attempt + 1))
+        sleep $wait_time
+    done
+
+    if [ $attempt -gt $retries ]; then
+        echo "Failed to install $extension after $retries attempts."
+        exit 1
+    fi
+}
+
+# Install base extensions
+base_extensions=(
+    "ms-toolsai.jupyter"
+    "ms-kubernetes-tools.vscode-kubernetes-tools"
+    "mhutchie.git-graph"
+    "hediet.vscode-drawio"
+    "streetsidesoftware.code-spell-checker"
+    "streetsidesoftware.code-spell-checker-french"
+)
+for extension in "${base_extensions[@]}"; do
+    install_extension $extension
+done
+
+# Python-specific configuration
+python_extensions=(
+    "ms-python.python"
+    "ms-python.flake8"
+)
+if command -v python &> /dev/null; then
+    for extension in "${python_extensions[@]}"; do
+        install_extension $extension
+    done
+    python_path=$(which python)
+    jq --arg pythonPath "$python_path" '.["python.defaultInterpreterPath"] = $pythonPath' ${REMOTE_CONFIG_DIR}/settings.json > tmp.json && mv tmp.json ${REMOTE_CONFIG_DIR}/settings.json
+fi
+
+# R-specific configuration
+r_extensions=(
+    "reditorsupport.r"
+    "RDebugger.r-debugger"
+)
+if command -v R &> /dev/null; then
+    for extension in "${r_extensions[@]}"; do
+        install_extension $extension
+    done
+    R -e "install.packages(c('languageserver', 'rmarkdown', 'httpgd'))"
+    R -e "remotes::install_github('ManuelHentschel/vscDebugger')"
+    pip install radian
+    r_path=$(which radian)
+    jq --arg rPath "$r_path" '.["r.rterm.linux"] = $rPath' ${REMOTE_CONFIG_DIR}/settings.json > tmp.json && mv tmp.json ${REMOTE_CONFIG_DIR}/settings.json
+fi
+
+# Julia-specific configuration
+if command -v julia &> /dev/null; then
+    install_extension "julialang.language-julia"
+fi
+
+# Quarto-specific configuration
+if command -v quarto &> /dev/null; then
+    install_extension "quarto.quarto"
+fi
