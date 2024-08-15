e1ca5cd17b865040db7a2e2dd97cc0ad08e62465
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2024-08-15 14:23:55 +0200

    refactor(build-pipeline): generate matrix for build jobs using a python script (#220)
    * first try lessgo
    
    * fix missing )
    
    * fix path
    
    * fix multiline cmd
    
    * fix missing param
    
    * fix missing '
    
    * add build_gpu parameter for releases
    
    * fix case when ubuntu base
    
    * fix case when ubuntu base
    
    * fix indent
    
    * fix missing '
    
    * fix missing '
    
    * fix build condition on gpu
    
    * default value for base gpu
    
    * fix duplicate
    
    * fiw arg names
    
    * generate tags in py script
    
    * fix ty^p
    
    * fix ty^p
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix prefix
    
    * fix prefix

---
+++ utils/generate-matrix.py
@@ -0,0 +1,93 @@
+"""Generate the matrix for each job of the build pipeline."""
+import json
+import argparse
+from datetime import datetime
+
+
+def generate_matrix(versions, input_image, output_image, spark_version,
+                    gpu_options, version_prefix):
+    """
+    Generate the build matrix for the given versions and options.
+
+    Args:
+        versions (list): A list of version strings (Python or R versions).
+        input_image (str): The base image name.
+        output_image (str): The output image name.
+        spark_version (str): The Spark version to include in the output image tag.
+        gpu_options (list): A list of booleans indicating whether to build GPU-enabled images.
+        version_prefix (str): A prefix to denote the language version ("py" or "r").
+
+    Returns:
+        list: A list of dictionaries, each representing a build configuration.
+    """
+    matrix = []
+    for version in versions:
+        base = f"{input_image}:latest" if input_image == "base" else f"{input_image}:{version_prefix}{version}"
+        output = f"{output_image}:{version_prefix}{version}"
+        language_key = "python_version" if version_prefix == "py" else "r_version"
+        version_entry = {"base_image_tag": f"{DH_ORGA}/{args.images_prefix}-{base}",
+                         "output_image_main_tag": f"{DH_ORGA}/{args.images_prefix}-{output}",
+                         language_key: version}
+        if spark_version:
+            version_entry["output_image_main_tag"] += f"-spark{spark_version}"
+            version_entry["spark_version"] = spark_version
+        for gpu in gpu_options:
+            final_entry = version_entry.copy()
+            suffix = "-gpu" if gpu else ""
+            final_entry["base_image_tag"] += suffix
+            final_entry["output_image_main_tag"] += suffix
+            final_entry["output_image_tags"] = f'{final_entry["output_image_main_tag"]},{final_entry["output_image_main_tag"]}-{TODAY_DATE}'
+            matrix.append(final_entry)
+    return matrix
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_image", type=str)
+    parser.add_argument("--output_image", type=str)
+    parser.add_argument("--python_version_1", type=str, nargs="?", const="")
+    parser.add_argument("--python_version_2", type=str, nargs="?", const="")
+    parser.add_argument("--r_version_1", type=str, nargs="?", const="")
+    parser.add_argument("--r_version_2", type=str, nargs="?", const="")
+    parser.add_argument("--spark_version", type=str, nargs="?", const="")
+    parser.add_argument("--build_gpu", type=str, nargs="?")
+    parser.add_argument("--base_image_gpu", type=str, nargs="?", const="")
+    parser.add_argument("--dh_orga", type=str)
+    parser.add_argument("--images_prefix", type=str)
+
+    args = parser.parse_args()
+    python_versions = [version for version in [args.python_version_1, args.python_version_2]
+                       if version]
+    r_versions = [version for version in [args.r_version_1, args.r_version_2] if version]
+    gpu_options = [False, True] if args.build_gpu == "true" else [False]
+
+    DH_ORGA = args.dh_orga.lower()
+    TODAY_DATE = datetime.today().strftime('%Y.%m.%d')
+
+    if args.output_image == "base":
+        # Building base onyxia image from external images
+        onyxia_base_tag = f"{args.images_prefix}-base:latest"
+        matrix = [
+            {
+                "base_image_tag": args.input_image,
+                "output_image_main_tag": f"{DH_ORGA}/{onyxia_base_tag}",
+                "output_image_tags": f"{DH_ORGA}/{onyxia_base_tag},{DH_ORGA}/{onyxia_base_tag}-{TODAY_DATE}"
+            },
+            {
+                "base_image_tag": args.base_image_gpu,
+                "output_image_main_tag": f"{DH_ORGA}/{onyxia_base_tag}-gpu",
+                "output_image_tags": f"{DH_ORGA}/{onyxia_base_tag}-gpu,{DH_ORGA}/{onyxia_base_tag}-gpu-{TODAY_DATE}"
+            }
+            ]
+    else:
+        # Subsequent images, with versioning
+        if python_versions:
+            matrix = generate_matrix(python_versions, args.input_image, args.output_image,
+                                     args.spark_version, gpu_options, "py")
+        elif r_versions:
+            matrix = generate_matrix(r_versions, args.input_image, args.output_image,
+                                     args.spark_version, gpu_options, "r")
+
+    matrix_json = json.dumps(matrix)
+    print(matrix_json)
