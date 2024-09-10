3bc21e68cfbc80e331f424deccff564fdf13c3bc
avouacr <avouacr@pm.me>
2024-09-10 08:52:54 +0000

    fix(all): add custom ppa for git to get up-to-date version

--- scripts/install-system-libs.sh
+++ scripts/install-system-libs.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 set -e
 
-function apt_install() {
-    if ! dpkg -s "$@" >/dev/null 2>&1; then
-        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-            apt-get update
-        fi
-        apt-get install -y --no-install-recommends "$@"
-    fi
-}
+# Add custom PPAs to get most up-to-date software
 
-apt_install \
+apt-get update
+apt-get install -y --no-install-recommends gnupg2 software-properties-common wget
+# PPA for git
+add-apt-repository -y ppa:git-core/ppa
+# PPA for postgresql-client
+echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+
+# Install system libraries
+apt-get update
+apt-get install -y --no-install-recommends \
     bash-completion \
     build-essential \
     ca-certificates \
     curl \
     git \
-    gnupg2 \
     jq \
     less \
     locales \
     lsb-core \
     nano \
     openssh-client \
+    postgresql-client \
     python3-pip \
     sudo \
     tini \
     unzip \
-    vim \
-    wget
-
-# Install postgresql-client
-echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt_install postgresql-client
+    vim
