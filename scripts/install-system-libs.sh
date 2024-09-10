2c4c686058789ec0142d75260700c5a645e0aa9a
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2024-09-10 12:07:09 +0200

    formatting

--- scripts/install-system-libs.sh
+++ scripts/install-system-libs.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Add custom PPAs to get most up-to-date software
-
 apt-get update
 apt-get install -y --no-install-recommends gnupg2 software-properties-common wget
 # PPA for git
@@ -10,7 +9,6 @@ add-apt-repository -y ppa:git-core/ppa
 # PPA for postgresql-client
 echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-
 
 # Install system libraries
 apt-get update
