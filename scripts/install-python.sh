aaeae236c9a28b5aef6eba008ccc7076981b1374
avouacr <avouacr@pm.me>
2025-03-29 22:14:35 +0000

    missing lib

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+# Install system libs
+apt_install \
+    zlib1g-dev
+
 # Install Python
 wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
 tar xzvf Python-${PYTHON_VERSION}.tgz
