54e80ab26fcf8327e0aa17e4771e7fb07d217228
avouacr <avouacr@pm.me>
2025-04-01 17:29:08 +0000

    fix path

--- utils/build-chains.sh
+++ utils/build-chains.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 dev/build-chain.py --chain vscode-python --push
-python3 dev/build-chain.py --chain vscode-pytorch --gpu --push
-python3 dev/build-chain.py --chain jupyter-tensorflow --gpu --push
-python3 dev/build-chain.py --chain rstudio --push
+python3 utils/build-chain.py --chain vscode-python --push
+python3 utils/build-chain.py --chain vscode-pytorch --gpu --push
+python3 utils/build-chain.py --chain jupyter-tensorflow --gpu --push
+python3 utils/build-chain.py --chain rstudio --push
