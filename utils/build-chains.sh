66c6aed5447b2e72c44d4a93b0d6a95ca00ca8f1
avouacr <avouacr@pm.me>
2025-04-02 08:12:26 +0000

    build logs to s33

--- utils/build-chains.sh
+++ utils/build-chains.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+LOGFILE=$(printf '%(%Y-%m-%d_%H:%M:%S)T\n' -1).log
+
 python3 utils/build-chain.py --chain vscode-python --push || true
 python3 utils/build-chain.py --chain vscode-pytorch --gpu --push || true
 python3 utils/build-chain.py --chain jupyter-tensorflow --gpu --push || true
 python3 utils/build-chain.py --chain rstudio --push || true
+
+mc cp nohup.out s3/projet-onyxia/build-images-datascience/$LOGFILE
+
 sudo shutdown -h now
