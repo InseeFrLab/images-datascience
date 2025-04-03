9904154c8e985d72d481a9ad1d0d03fe9979244c
avouacr <avouacr@pm.me>
2025-04-04 01:55:47 +0200

    improve r

--- scripts/install-r-minimal-packages.sh
+++ scripts/install-r-minimal-packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# Install required system libraries
+
 function apt_install() {
     if ! dpkg -s "$@" >/dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -14,3 +16,19 @@ apt_install \
     libglpk40 \
     libpq-dev \
     libzmq3-dev
+
+# Install R packages
+
+install2.r --ncpus -1 --error \
+    arrow \
+    aws.s3 \
+    devtools \
+    DBI \
+    duckdb \
+    lintr \
+    paws \
+    quarto \
+    renv \
+    RPostgreSQL \
+    styler \
+    targets
