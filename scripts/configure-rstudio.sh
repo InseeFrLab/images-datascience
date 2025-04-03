94088478eb5a1407945881c3ce6add7327d78557
avouacr <avouacr@pm.me>
2025-04-04 01:18:50 +0200

    improve rstudio

---
+++ scripts/configure-rstudio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+RSTUDIO_SYSTEM_CONF_FILE="/etc/rstudio/rsession.conf"
+RSTUDIO_USER_CONF_FILE="${HOME}/.config/rstudio/rstudio-prefs.json"
+
+# Set default working directory for R sessions and R projects
+echo "session-default-working-dir=${WORKSPACE_DIR}" >> ${RSTUDIO_SYSTEM_CONF_FILE}
+echo "session-default-new-project-dir=${WORKSPACE_DIR}" >> ${RSTUDIO_SYSTEM_CONF_FILE}
+# Initialize RStudio's user config file
+mkdir -p "$(dirname $RSTUDIO_USER_CONF_FILE)"
+echo '{}' > $RSTUDIO_USER_CONF_FILE
