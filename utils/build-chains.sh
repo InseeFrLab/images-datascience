582613d8e4728a49f8f12eea9b831685c4cf3711
avouacr <avouacr@pm.me>
2025-04-04 10:39:35 +0000

    shutdown

--- utils/build-chains.sh
+++ utils/build-chains.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
 
 LOGFILE=$(printf '%(%Y-%m-%d_%H:%M:%S)T\n' -1).log
 
 python3 utils/build-chain.py --chain vscode-python --version 3.12.9 --push
 python3 utils/build-chain.py --chain rstudio --version 4.4.2 --push
-python3 utils/build-chain.py --chain vscode-pytorch --version 3.12.9 --gpu --push
-python3 utils/build-chain.py --chain jupyter-tensorflow -- version 3.12.9 --gpu --push
+# python3 utils/build-chain.py --chain vscode-pytorch --version 3.12.9 --gpu --push
+# python3 utils/build-chain.py --chain jupyter-tensorflow -- version 3.12.9 --gpu --push
 
 mc cp nohup.out s3/projet-onyxia/build-images-datascience/$LOGFILE
+
+shutdown -h now
