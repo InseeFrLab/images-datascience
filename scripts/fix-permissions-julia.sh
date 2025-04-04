caf748b016731896a7373fadad6fa27731e6a205
avouacr <avouacr@pm.me>
2025-04-04 22:58:36 +0200

    fix home permissions

--- scripts/fix-permissions-julia.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-chown -R ${USERNAME}:${GROUPNAME} ${HOME}/.julia/
--- scripts/fix-permissions-python.sh
+++ scripts/fix-permissions-python.sh
@@ -2,4 +2,3 @@
 set -e
 
 chown -R ${USERNAME}:${GROUPNAME} /usr/local/lib/python${PYTHON_VERSION%.*}/site-packages/
-chown -R ${USERNAME}:${GROUPNAME} ${HOME}/.cache/
