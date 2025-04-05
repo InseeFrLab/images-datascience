c467deca9e2b358037ef3f849f5ce912ba26e11f
avouacr <avouacr@pm.me>
2025-04-05 12:33:41 +0200

    improve permissions with conditional chow

--- scripts/fix-user-permissions.sh
+++ scripts/fix-user-permissions.sh
@@ -4,3 +4,4 @@ set -e
 # Chown only if needed to avoid duplicate files in Docker layers
 find "${HOME}" -not -user "${USERNAME}" -execdir chown --no-dereference "${USERNAME}:${GROUPNAME}" {} \+
 find /usr/local/ -not -user "${USERNAME}" -execdir chown --no-dereference "${USERNAME}:${GROUPNAME}" {} \+
+find /opt/ -not -user "${USERNAME}" -execdir chown --no-dereference "${USERNAME}:${GROUPNAME}" {} \+
