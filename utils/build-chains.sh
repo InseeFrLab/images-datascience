2e5aac5c486da145457e3be5b0ad1da14485c739
avouacr <avouacr@pm.me>
2025-04-07 23:42:29 +0200

    add shutdown

--- utils/build-chains.sh
+++ utils/build-chains.sh
@@ -2,6 +2,7 @@
 set -e
 
 PUSH=true
+SHUTDOWN=true
 
 PUSH_ARG=""
 if [ "$PUSH" = "true" ]; then
@@ -13,7 +14,11 @@ echo $PUSH_ARG
 # Commands
 python3 utils/build-chain.py --chain vscode-python --py_version 3.12.9 $PUSH_ARG
 python3 utils/build-chain.py --chain rstudio --r_version 4.4.2 $PUSH_ARG
+python3 utils/build-chain.py --chain jupyter-r-python-julia --r_version 4.4.2 --py_version 3.12.9 $PUSH_ARG
 python3 utils/build-chain.py --chain vscode-pytorch --py_version 3.12.9 --gpu $PUSH_ARG
 python3 utils/build-chain.py --chain vscode-tensorflow --py_version 3.12.9 --gpu $PUSH_ARG
-python3 utils/build-chain.py --chain jupyter-r-python-julia --r_version 4.4.2 --py_version 3.12.9 $PUSH_ARG
 python3 utils/build-chain.py --chain jupyter-pyspark --py_version 3.12.9 --spark_version 3.5.5 $PUSH_ARG
+
+if [ "$SHUTDOWN" = "true" ]; then
+  sudo shutdown -h now 
+fi
