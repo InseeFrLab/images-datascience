3c4bdcdb12eafe2fdb07ead0ffac8e2881e30a5d
phlg <76954542+phlg@users.noreply.github.com>
2026-01-19 16:11:46 +0100

    fix: kubectl install command (#346)
    * fix: kubectl install command
    
    * fix: wrong URL
    
    (hardcoded architecture + paste mistake)

--- base/scripts/install-kubectl.sh
+++ base/scripts/install-kubectl.sh
@@ -16,7 +16,7 @@ case $ARCH in
         ;;
 esac
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/$ARCHITECTURE/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$ARCHITECTURE/kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 echo 'source <(kubectl completion bash)' >> ${HOME}/.bashrc
