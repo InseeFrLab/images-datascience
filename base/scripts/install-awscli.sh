0c1855e91ef9f1c512a6cce5d036f5fd7ef7e692
avouacr <avouacr@pm.me>
2026-09-20 07:32:59 +0000

    fix cmd

--- base/scripts/install-awscli.sh
+++ base/scripts/install-awscli.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -e
 
-ARCH=$(uname -m)
+# renovate: datasource=github-tags depName=aws/aws-cli
+AWS_CLI_VERSION="2.36.49"
 
-case $ARCH in
-    "x86_64")
-        DIRECTORY="linux-x86_64"
-        ;;
-    "aarch64")
-        DIRECTORY="linux-aarch64"
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-        ;;
-esac
-curl "https://awscli.amazonaws.com/awscli-exe-$DIRECTORY.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install 
+# Download the official archive and its PGP signature
+AWS_CLI_ZIP="awscli-exe-linux-x86_64-${AWS_CLI_VERSION}.zip"
+TMP_DIR=$(mktemp -d)
+curl -fsSL "https://awscli.amazonaws.com/${AWS_CLI_ZIP}" -o "${TMP_DIR}/${AWS_CLI_ZIP}"
+curl -fsSL "https://awscli.amazonaws.com/${AWS_CLI_ZIP}.sig" -o "${TMP_DIR}/${AWS_CLI_ZIP}.sig"
+
+# Install AWS CLI
+unzip -q "${TMP_DIR}/${AWS_CLI_ZIP}" -d "${TMP_DIR}"
+"${TMP_DIR}/aws/install"
 chmod +x /usr/local/bin/aws
-echo "source <(complete -C '/usr/local/bin/aws_completer' aws )" >> ${HOME}/.bashrc
+rm -rf "${TMP_DIR}"
+
+# Activate autocomplete in the CLI
+echo "complete -C '/usr/local/bin/aws_completer' aws" >> ~/.bashrc
